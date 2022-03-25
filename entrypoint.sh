559cf727a21efd5834d737aeafce398641ae4f1c
Robin Duncan Sharp <rob@sharp.id.au>
2022-03-25 12:32:12 +1100

    Capture the convox release ID in the environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,12 @@ then
  export CONVOX_HOST=$INPUT_HOST
 fi
 export CONVOX_RACK=$INPUT_RACK
-convox deploy --app $INPUT_APP --description "$INPUT_DESCRIPTION" --wait
+release=$(convox deploy --app $INPUT_APP --description "$INPUT_DESCRIPTION" --wait --id)
+
+if [ -z "$release" ]
+then
+  echo "Deploy failed"
+  exit 1
+fi
+echo ::set-output name=release::$release
+echo "RELEASE=$release" >> $GITHUB_ENV
